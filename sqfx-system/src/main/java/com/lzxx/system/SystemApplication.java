c9f305bbbcd8725b773f4c92c7edfb8b383b53c1
先谢郭嘉 <xie430423@163.com>
2021-03-02 09:06:17 +0800

    commit

--- sqfx-system/src/main/java/com/lzxx/system/SystemApplication.java
+++ sqfx-system/src/main/java/com/lzxx/system/SystemApplication.java
@@ -1,5 +1,6 @@
 package com.lzxx.system;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,10 +19,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.lzxx.**.mapper")
 @EnableTransactionManagement
 @EnableScheduling // 开启定时任务支持
+@Slf4j
 public class SystemApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  社区飞信 SpringBoot 后端启动成功   ლ(´ڡ`ლ)ﾞ");
+        log.info("(♥◠‿◠)ﾉﾞ  社区飞信 SpringBoot 后端启动成功   ლ(´ڡ`ლ)ﾞ");
+        log.info("==================当前项目目录 {} ================",System.getProperty("user.dir"));
     }
 
     @Override
